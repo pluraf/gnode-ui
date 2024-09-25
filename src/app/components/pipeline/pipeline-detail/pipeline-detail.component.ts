dfd1c0b66971a7601f14cb7ae1f1f28e3d4fa6e0
Supriya Sinha <supriya@pluraf.com>
2024-09-26 00:50:19 +0200

    Fixed background view of the logged-in user dialog box

---
+++ src/app/components/pipeline/pipeline-detail/pipeline-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipeline-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './pipeline-detail.component.html',
+  styleUrl: './pipeline-detail.component.css'
+})
+export class PipelineDetailComponent {
+
+}
